32cedf21be7535fe86b4cbfcae9b4f1dd6a46ce9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-10-23 17:23:09 +0200

    trying a thing, but it failed

--- autodocodec-aeson/src/Autodocodec/Aeson/Document.hs
+++ autodocodec-aeson/src/Autodocodec/Aeson/Document.hs
@@ -11,17 +11,19 @@ module Autodocodec.Aeson.Document where
 import Autodocodec
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Aeson as JSON
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
+-- http://json-schema.org/understanding-json-schema/reference/index.html
 data JSONSchema
   = AnySchema
   | NullSchema
   | BoolSchema
   | StringSchema
   | NumberSchema
-  | -- | ValueSchema JSON.Value
-    ObjectSchema !JSONObjectSchema
+  | ObjectSchema !JSONObjectSchema
   | ChoiceSchema ![JSONSchema]
   deriving (Show, Eq, Generic)
 
@@ -40,18 +42,61 @@ instance ToJSON JSONSchema where
     StringSchema -> JSON.object ["type" JSON..= ("string" :: Text)]
     NumberSchema -> JSON.object ["type" JSON..= ("number" :: Text)]
     ChoiceSchema jcs -> JSON.object ["anyOf" JSON..= jcs]
-    ObjectSchema _ -> JSON.object ["type" JSON..= ("object" :: Text)] -- TODO this is wrong
+    ObjectSchema os ->
+      let go = \case
+            AnyObjectSchema -> []
+            KeySchema k s -> [([(k, s)], [k])]
+            ApObjectSchema os1 os2 -> do
+              (ps1, rps1) <- go os1
+              (ps2, rps2) <- go os2
+              pure $ (ps1 ++ ps2, rps1 ++ rps2)
+            ChoiceObjectSchema cs -> concatMap go cs
+          schemaForTup (ps, rps) = case rps of
+            [] ->
+              JSON.object
+                [ "type" JSON..= ("object" :: Text),
+                  "properties" JSON..= ps
+                ]
+            _ ->
+              JSON.object
+                [ "type" JSON..= ("object" :: Text),
+                  "properties" JSON..= ps,
+                  "required" JSON..= rps
+                ]
+       in case go os of
+            [] -> JSON.object ["type" JSON..= ("object" :: Text)] -- TODO this is wrong
+            [tup] -> schemaForTup tup
+            tups -> JSON.object ["anyOf" JSON..= map schemaForTup tups]
 
 instance FromJSON JSONSchema where
   parseJSON = JSON.withObject "JSONSchema" $ \o -> do
-    t <- o JSON..: "type"
-    case t :: Text of
-      "null" -> pure NullSchema
-      "boolean" -> pure BoolSchema
-      "string" -> pure StringSchema
-      "number" -> pure NumberSchema
-      "object" -> pure $ ObjectSchema AnyObjectSchema
-      _ -> fail "unknown schema type."
+    mt <- o JSON..:? "type"
+    case mt :: Maybe Text of
+      Just "null" -> pure NullSchema
+      Just "boolean" -> pure BoolSchema
+      Just "string" -> pure StringSchema
+      Just "number" -> pure NumberSchema
+      Just "object" -> do
+        mP <- o JSON..: "properties"
+        case mP of
+          Nothing -> pure $ ObjectSchema AnyObjectSchema
+          Just props -> do
+            -- _ <- fromMaybe [] <$> o JSON..:? "required"
+            -- TODO distinguish between required and optional properties
+            let keySchemas = map (\(k, s) -> KeySchema k s) props
+            let go (ks :| rest) = case NE.nonEmpty rest of
+                  Nothing -> ks
+                  Just ne -> ApObjectSchema ks (go ne)
+            pure $
+              ObjectSchema $ case NE.nonEmpty keySchemas of
+                Nothing -> AnyObjectSchema
+                Just ne -> go ne
+      Nothing -> do
+        mAny <- o JSON..:? "anyOf"
+        case mAny of
+          Just anies -> pure $ ChoiceSchema anies
+          Nothing -> fail "Unknown object schema without type."
+      t -> fail $ "unknown schema type:" <> show t
 
 jsonSchemaViaCodec :: forall a. HasCodec a => JSONSchema
 jsonSchemaViaCodec = jsonSchemaVia (codec @a)
--- autodocodec-aeson/test/Autodocodec/Aeson/DocumentSpec.hs
+++ autodocodec-aeson/test/Autodocodec/Aeson/DocumentSpec.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -8,8 +10,11 @@ import Autodocodec
 import Autodocodec.Aeson
 import Data.Data
 import Data.GenValidity
+import Data.GenValidity.Scientific ()
 import Data.GenValidity.Text ()
+import Data.Scientific
 import Data.Text (Text)
+import GHC.Generics (Generic)
 import Test.Syd
 import Test.Syd.Aeson
 import Test.Syd.Validity.Utils
@@ -18,9 +23,31 @@ spec :: Spec
 spec = do
   jsonSchemaSpec @Bool "bool"
   jsonSchemaSpec @Text "text"
+  jsonSchemaSpec @Scientific "scientific"
   jsonSchemaSpec @(Either Bool Text) "either-bool-text"
+  jsonSchemaSpec @(Either (Either Bool Scientific) Text) "either-either-bool-scientific-text"
+  jsonSchemaSpec @Example "example"
 
 jsonSchemaSpec :: forall a. (Show a, Eq a, Typeable a, GenValid a, HasCodec a) => FilePath -> Spec
 jsonSchemaSpec filePath = do
   it ("outputs the same schema as before for " <> nameOf @a) $
-    pureGoldenJSONValueFile ("test_resources/" <> filePath <> ".json") (jsonSchemaViaCodec @a)
+    pureGoldenJSONValueFile ("test_resources/schema/" <> filePath <> ".json") (jsonSchemaViaCodec @a)
+
+data Example = Example
+  { exampleText :: !Text,
+    exampleBool :: !Bool
+  }
+  deriving (Show, Eq, Generic)
+
+instance Validity Example
+
+instance GenValid Example where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
+
+instance HasCodec Example where
+  codec =
+    object $
+      Example
+        <$> field "text" .= exampleText
+        <*> field "bool" .= exampleBool
