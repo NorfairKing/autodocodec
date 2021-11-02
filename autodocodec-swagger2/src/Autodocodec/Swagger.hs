e69903fa9ef85a0ba04437009a1c820db9c4d046
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-11-02 18:04:41 +0100

    trying to make swagger schemas for object codecs as well

--- autodocodec-swagger2/src/Autodocodec/Swagger.hs
+++ autodocodec-swagger2/src/Autodocodec/Swagger.hs
@@ -6,6 +6,7 @@
 module Autodocodec.Swagger where
 
 import Autodocodec
+import Autodocodec.Aeson.Encode
 import Control.Monad
 import qualified Data.Aeson as JSON
 import qualified Data.HashMap.Strict.InsOrd as InsOrdHashMap
@@ -14,7 +15,6 @@ import Data.Scientific
 import Data.Swagger as Swagger
 import Data.Swagger.Declare as Swagger
 import Data.Text (Text)
-import Debug.Trace
 
 declareNamedSchemaViaCodec :: HasCodec value => Proxy value -> Declare (Definitions Schema) NamedSchema
 declareNamedSchemaViaCodec proxy = declareNamedSchemaVia codec proxy
@@ -22,13 +22,12 @@ declareNamedSchemaViaCodec proxy = declareNamedSchemaVia codec proxy
 declareNamedSchemaVia :: JSONCodec value -> Proxy value -> Declare (Definitions Schema) NamedSchema
 declareNamedSchemaVia c' Proxy = go c'
   where
-    go :: Codec context input output -> Declare (Definitions Schema) NamedSchema
+    go :: ValueCodec input output -> Declare (Definitions Schema) NamedSchema
     go = \case
       BoolCodec mname -> NamedSchema mname <$> declareSchema (Proxy :: Proxy Bool)
       StringCodec mname -> NamedSchema mname <$> declareSchema (Proxy :: Proxy Text)
       NumberCodec mname -> NamedSchema mname <$> declareSchema (Proxy :: Proxy Scientific)
       ArrayOfCodec mname c -> do
-        traceM "hello"
         itemsSchema <- go c
         itemsSchemaRef <- declareSpecificNamedSchemaRef itemsSchema
         pure $
@@ -40,6 +39,9 @@ declareNamedSchemaVia c' Proxy = go c'
                       _paramSchemaItems = Just $ SwaggerItemsObject $ _namedSchemaSchema <$> itemsSchemaRef
                     }
               }
+      ObjectOfCodec mname oc -> do
+        ss <- goObject oc
+        pure $ NamedSchema mname $ mconcat ss
       ObjectCodec -> declareNamedSchema (Proxy :: Proxy JSON.Object)
       MapCodec _ _ c -> go c
       CommentCodec t c -> do
@@ -57,6 +59,43 @@ declareNamedSchemaVia c' Proxy = go c'
             pure ns
           Just s -> pure $ NamedSchema (Just n) s
       _ -> pure $ NamedSchema Nothing mempty -- TODO
+    goObject :: ObjectCodec input output -> Declare (Definitions Schema) [Schema]
+    goObject = \case
+      RequiredKeyCodec key vs mDoc -> do
+        ns <- go vs
+        ref <- declareSpecificNamedSchemaRef ns
+        pure
+          [ mempty
+              { _schemaDescription = mDoc,
+                _schemaRequired = [key],
+                _schemaProperties = [(key, _namedSchemaSchema <$> ref)]
+              }
+          ]
+      OptionalKeyCodec key vs mDoc -> do
+        ns <- go vs
+        ref <- declareSpecificNamedSchemaRef ns
+        pure
+          [ mempty
+              { _schemaDescription = mDoc, -- TODO the docs should probably go in the ns?
+                _schemaProperties = [(key, _namedSchemaSchema <$> ref)]
+              }
+          ]
+      OptionalKeyWithDefaultCodec key vs defaultValue mDoc -> do
+        ns <- go vs
+        ref <- declareSpecificNamedSchemaRef ns
+        pure
+          [ mempty
+              { _schemaDescription = mDoc,
+                _schemaProperties = [(key, _namedSchemaSchema <$> ref)],
+                _schemaParamSchema = mempty {_paramSchemaDefault = Just $ toJSONVia vs defaultValue}
+              }
+          ]
+      PureCodec _ -> pure []
+      ApCodec oc1 oc2 -> do
+        ss1 <- goObject oc1
+        ss2 <- goObject oc2
+        pure $ ss1 ++ ss2
+      _ -> pure []
 
 declareSpecificNamedSchemaRef :: Swagger.NamedSchema -> Declare (Definitions Schema) (Referenced NamedSchema)
 declareSpecificNamedSchemaRef namedSchema =
