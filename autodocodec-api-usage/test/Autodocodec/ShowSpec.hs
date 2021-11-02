26bafc1696b7f329f3abfd1ac144a43245d3ccdb
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-11-02 21:55:11 +0100

    golden test for the show output

---
+++ autodocodec-api-usage/test/Autodocodec/ShowSpec.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Autodocodec.ShowSpec (spec) where
+
+import Autodocodec
+import Autodocodec.Aeson
+import Autodocodec.Usage
+import Data.Aeson (FromJSON (..), ToJSON (..))
+import qualified Data.Aeson as JSON
+import qualified Data.Aeson.Types as JSON
+import Data.Data
+import Data.GenValidity
+import Data.GenValidity.Aeson ()
+import Data.GenValidity.Scientific ()
+import Data.GenValidity.Text ()
+import Data.Int
+import Data.Scientific
+import Data.Text (Text)
+import qualified Data.Text.Lazy as LT
+import Data.Word
+import Test.Syd
+import Test.Syd.Validity
+import Test.Syd.Validity.Utils
+
+spec :: Spec
+spec = do
+  showCodecSpec @Bool "bool"
+  showCodecSpec @Ordering "ordering"
+  showCodecSpec @Char "char"
+  showCodecSpec @Text "text"
+  showCodecSpec @LT.Text "lazy-text"
+  showCodecSpec @String "string"
+  showCodecSpec @Scientific "scientific"
+  showCodecSpec @JSON.Value "value"
+  showCodecSpec @Int "int"
+  showCodecSpec @Int8 "int8"
+  showCodecSpec @Int16 "int16"
+  showCodecSpec @Int32 "int32"
+  showCodecSpec @Int64 "int64"
+  showCodecSpec @Word "word"
+  showCodecSpec @Word8 "word8"
+  showCodecSpec @Word16 "word16"
+  showCodecSpec @Word32 "word32"
+  showCodecSpec @Word64 "word64"
+  showCodecSpec @(Maybe Text) "maybe-text"
+  showCodecSpec @(Either Bool Text) "either-bool-text"
+  showCodecSpec @(Either (Either Bool Scientific) Text) "either-either-bool-scientific-text"
+  showCodecSpec @[Text] "list-text"
+  showCodecSpec @Fruit "fruit"
+  showCodecSpec @Example "example"
+  showCodecSpec @Recursive "recursive"
+  showCodecSpec @Via "via"
+
+showCodecSpec :: forall a. (Show a, Eq a, Typeable a, GenValid a, HasCodec a) => FilePath -> Spec
+showCodecSpec filePath =
+  describe ("showCodecSpec " <> nameOf @a) $
+    it "outputs the same shown codec information as before" $
+      pureGoldenStringFile ("test_resources/show-codec/" <> filePath <> ".txt") (showCodecABit (codec @a))
