a6134dcdb95c523cb7e6cec05f568e90d446c5ce
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-11-05 19:45:18 +0100

    error message golden tests

--- autodocodec-api-usage/test/Autodocodec/AesonSpec.hs
+++ autodocodec-api-usage/test/Autodocodec/AesonSpec.hs
@@ -11,6 +11,7 @@ import Autodocodec.Usage
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Aeson as JSON
 import qualified Data.Aeson.Types as JSON
+import qualified Data.ByteString.Lazy as LB
 import Data.Data
 import Data.GenValidity
 import Data.GenValidity.Aeson ()
@@ -29,35 +30,59 @@ import Test.Syd.Validity.Utils
 spec :: Spec
 spec = do
   aesonCodecSpec @NullUnit
+  aesonCodecErrorSpec @NullUnit "null-error-number" "5"
   aesonCodecSpec @Bool
+  aesonCodecErrorSpec @Bool "bool-error-string" "\"hello\""
   aesonCodecSpec @Ordering
+  aesonCodecErrorSpec @Ordering "ordering-error-list" "[\"foo\"]"
   xdescribe "does not hold" $ aesonCodecSpec @Char
+  aesonCodecErrorSpec @Char "char-error-number" "6"
   aesonCodecSpec @Text
+  aesonCodecErrorSpec @Text "text-error-bool" "true"
   aesonCodecSpec @LT.Text
+  aesonCodecErrorSpec @LT.Text "lazy-text-error-null" "null"
   xdescribe "does not hold" $ aesonCodecSpec @String
+  aesonCodecErrorSpec @String "string-error-object" "{}"
   aesonCodecSpec @Scientific
+  aesonCodecErrorSpec @Scientific "scientific-error-string" "\"quux\""
   aesonCodecSpec @JSON.Object
+  aesonCodecErrorSpec @JSON.Object "object-error-array" "[5, 6]"
   aesonCodecSpec @JSON.Value
   aesonCodecSpec @Int
+  aesonCodecErrorSpec @Int "int-error-big" "1E10000"
   aesonCodecSpec @Int8
   aesonCodecSpec @Int16
   aesonCodecSpec @Int32
   aesonCodecSpec @Int64
   aesonCodecSpec @Word
+  aesonCodecErrorSpec @Word "word-error-negative" "-3"
   aesonCodecSpec @Word8
   aesonCodecSpec @Word16
   aesonCodecSpec @Word32
   aesonCodecSpec @Word64
   aesonCodecSpec @(Maybe Text)
+  aesonCodecErrorSpec @(Maybe Text) "maybe-text-error-object" "{}"
   aesonCodecSpec @(Either Bool Text)
+  aesonCodecErrorSpec @(Either Bool Text) "either-bool-text-error-array" "[{}]"
   aesonCodecSpec @(Either (Either Bool Scientific) Text)
   aesonCodecSpec @[Text]
+  aesonCodecErrorSpec @[Text] "list-text-error-string" "\"string\""
   aesonCodecSpec @(NonEmpty Text)
   aesonCodecSpec @Fruit
   aesonCodecSpec @Example
+  aesonCodecErrorSpec @Example "example-error-bool-number" "{\"text\": \"hello\", \"bool\": 5}"
   aesonCodecSpec @Recursive
   aesonCodecSpec @Via
   aesonCodecSpec @VeryComment
+
+aesonCodecErrorSpec :: forall a. (Show a, Eq a, Typeable a, GenValid a, HasCodec a) => FilePath -> LB.ByteString -> Spec
+aesonCodecErrorSpec filePath encoded =
+  it "shows the same error as before" $
+    goldenStringFile ("test_resources/error/" <> filePath <> ".txt") $ do
+      let decodedWithAutodocodec = JSON.eitherDecode encoded
+      case decodedWithAutodocodec of
+        Left err -> pure err
+        Right (Autodocodec (_ :: a)) -> expectationFailure "Should not have succeeded."
 
 aesonCodecSpec :: forall a. (Show a, Eq a, Typeable a, GenValid a, ToJSON a, FromJSON a, HasCodec a) => Spec
 aesonCodecSpec =
