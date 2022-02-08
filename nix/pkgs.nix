303824f637b22781c86428b4958b0d5b03e54d2f
Sridhar Ratnakumar <srid@srid.ca>
2022-02-08 16:56:02 -0500

    Update sydtest, and re-enable its overlay

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -8,7 +8,7 @@ let
         (import (sources.validity + "/nix/overlay.nix"))
         (import (sources.safe-coloured-text + "/nix/overlay.nix"))
         (import (sources.yamlparse-applicative + "/nix/overlay.nix"))
-        # (import (sources.sydtest + "/nix/overlay.nix"))
+        (import (sources.sydtest + "/nix/overlay.nix"))
         (final: previous: { inherit (import sources.gitignore { inherit (final) lib; }) gitignoreSource; })
         (import ./overlay.nix)
       ];
