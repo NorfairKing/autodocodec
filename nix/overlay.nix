3ae5182fcc7c695ed53e64fd4f64526505803be7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-06-28 13:38:52 +0200

    Extra GHC flags on CI, and do not error on deprecations

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,7 +5,29 @@ with final.haskell.lib;
   autodocodecPackages =
     let
       autodocodecPkg = name:
-        doBenchmark (buildStrictly (final.haskellPackages.callCabal2nixWithOptions name (final.gitignoreSource (../. + "/${name}")) "--no-hpack" { }));
+        buildStrictly (
+          overrideCabal (final.haskellPackages.callCabal2nixWithOptions name (final.gitignoreSource (../. + "/${name}")) "--no-hpack" { })
+            (old: {
+              doBenchmark = true;
+              configureFlags = (old.configureFlags or [ ]) ++ [
+                # Optimisations
+                "--ghc-options=-O2"
+                # Extra warnings
+                "--ghc-options=-Wall"
+                "--ghc-options=-Wincomplete-uni-patterns"
+                "--ghc-options=-Wincomplete-record-updates"
+                "--ghc-options=-Wpartial-fields"
+                "--ghc-options=-Widentities"
+                "--ghc-options=-Wredundant-constraints"
+                "--ghc-options=-Wcpp-undef"
+                "--ghc-options=-Werror"
+                "--ghc-options=-Wno-deprecations"
+              ];
+              # Ugly hack because we can't just add flags to the 'test' invocation.
+              # Show test output as we go, instead of all at once afterwards.
+              testTarget = (old.testTarget or "") + " --show-details=direct";
+            })
+        );
     in
     final.lib.genAttrs [
       "autodocodec"
