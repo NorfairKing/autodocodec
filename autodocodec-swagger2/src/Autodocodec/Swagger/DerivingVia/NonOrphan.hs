fcc158682365fd7fcb9767ddcbfe0bb14cf7ec9b
Clinton Mead <clinton.mead@paidright.io>
2024-07-29 22:30:38 +1000

    Add non-orphan versions of deriving via instances for OpenAPI and Swagger

---
+++ autodocodec-swagger2/src/Autodocodec/Swagger/DerivingVia/NonOrphan.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Autodocodec.Swagger.DerivingVia.NonOrphan (AutodocodecSwagger (..)) where
+
+import Autodocodec (HasCodec)
+import Autodocodec.Swagger.Schema (declareNamedSchemaViaCodec)
+import Data.Proxy (Proxy (..))
+import qualified Data.Swagger as Swagger
+
+newtype AutodocodecSwagger a = AutodocodecSwagger {unAutodocodecSwagger :: a}
+
+-- | An non-orphan instance for 'AutodocodecSwagger' that lets you use 'DerivingVia' to derive 'Swagger.ToSchema' if your type has a 'HasCodec' instance.
+--
+-- > deriving (Swagger.ToSchema) via (AutodocodecSwagger FooBar)
+instance (HasCodec a) => Swagger.ToSchema (AutodocodecSwagger a) where
+  -- See comments in 'Autodocodec.OpenAPI.DerivingVia' for the reason why this is defined like this.
+  declareNamedSchema = let schema = declareNamedSchemaViaCodec (Proxy :: Proxy a) in const schema
